d35f0c0a5c18981f58f21884ab7f1ab017f3c036
eilo2010 <huidudaima@gmail.com>
2025-08-21 19:32:28 +0800

    更新内容

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_URL = "https://aptu.net";
 export const SITE_TITLE = "aptU";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Keep Upgrading";
 
 export const EMAIL = "8361207@gmail.com";
